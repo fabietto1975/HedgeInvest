31620a92c05cbe6c35332ec8a188e6374f250076
fabietto1975 <fabio.surini@gmail.com>
2016-11-04 13:11:55 +0100

    Prova

--- it/valuelab/hedgeinvest/KIID/service/KIIDService.cs
+++ it/valuelab/hedgeinvest/KIID/service/KIIDService.cs
@@ -90,6 +90,7 @@ namespace it.valuelab.hedgeinvest.KIID.service
                 wordHelper.replaceText("@SPESEDISOTTOSCRIZIONE@", string.Format("{0} %", data.SpeseSottoscrizione));
                 wordHelper.replaceText("@TESTO1@", data.Testo1);
                 System.Diagnostics.Debug.WriteLine(data.Testo1);
+                //wordHelper.replaceText("@TESTO1@", "\t\u2022 Riga 1 \u000a\u000d\u2022 Riga 2");
                 //wordHelper.replaceText("@GRAFICO@", "AA");
                 wordHelper.InsertProfiloRischio(data.ClasseDiRischio);
                 if (data.Performances != null)
